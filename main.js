bf5c5e01f015d7f0a4da563fecdc0ad81757b95e
015Tommy <302181697@student.rocmondriaan.nl>
2024-10-23 12:07:19 +0200

    egdrgdr

---
+++ main.js
@@ -0,0 +1,11 @@
+let scoreA = 85;
+let scoreB = 90;
+
+
+if (scoreA > scoreB) {
+    console.log("Winnaar: Score A");
+} else if (scoreA < scoreB) {
+    console.log("Winnaar: Score B");
+} else {
+    console.log("Gelijkspel!");
+}
